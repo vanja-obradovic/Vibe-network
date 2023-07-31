84c75c8e6579e91095cd6000d6721f9665bab104
Nikola Grujičić <nikola.grujicic@mvpworkshop.co>
2023-07-31 13:59:12 +0200

    refactor: added tailwind styling

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="bg-green-600">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -21,7 +21,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="font-bold">
         Click on the Vite and React logos to learn more
       </p>
     </>
