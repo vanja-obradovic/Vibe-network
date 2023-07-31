f24cc392fdcf0a8081e72e675cf443d68a600e2b
Nikola Grujičić <nikola.grujicic@mvpworkshop.co>
2023-07-31 13:35:56 +0200

    refactor: fixed svg import

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
-/// <reference types="vite/client" />
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.woff2";
+declare module "*.woff";
+declare module "*.ttf";
+
+declare module "*.svg" {
+  import React = require("react");
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
