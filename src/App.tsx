9608708733871ac4e491ed8ec491792cff00567e
Nikola Grujičić <nikola.grujicic@mvpworkshop.co>
2023-07-31 16:17:25 +0200

    style: fixed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="font-bold">
+      <p className="text-3xl font-bold underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
