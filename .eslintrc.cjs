45de9bd18dd28a008b973b627e0a47df7d43d06f
npanajotov <11493864+npanajotov@users.noreply.github.com>
2023-08-02 19:46:30 +0200

    feat: remove some files, added missing packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,60 @@
-module.exports = {
-  extends: [
+{
+  "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:import/recommended",
     "plugin:@typescript-eslint/recommended",
     "eslint-config-prettier",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended"
   ],
-  settings: {
-    react: {
-      version: "detect",
+  "plugins": [
+    "react",
+    "react-hooks",
+    "@typescript-eslint",
+    "prettier",
+    "jsx-a11y",
+    "@tanstack/query"
+  ],
+  "settings": {
+    "react": {
+      "version": "detect"
     },
     "import/resolver": {
-      node: {
-        paths: ["src"],
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
+      "node": {
+        "paths": [
+          "src"
+        ],
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
   },
-  rules: {
-    // Add your own rules here to override ones from the extended configs.
+  "rules": {
+    "react/react-in-jsx-scope": "off",
+    "react/no-unescaped-entities": "off",
+    "camelcase": [
+      "error",
+      {
+        "allow": []
+      }
+    ],
+    "eqeqeq": "error",
+    "spaced-comment": "error",
+    "no-empty-interface": "off",
+    "no-var-requires": "off",
+    "no-duplicate-imports": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "arrow-spacing": "error",
+    "block-spacing": "error"
   },
-  env: {
-    node: true,
-  },
-};
+  "env": {
+    "node": true
+  }
+}
